dc65c3e770bd3315221100da8e62988ad7dd97a2
SlaveFact0ry <guppy0118@gmail.com>
2025-11-16 14:18:31 +0900

    adding vectors with multiple blocks

--- adding_vectors/adding_vectors.cu
+++ adding_vectors/adding_vectors.cu
@@ -17,34 +17,40 @@ void printVector(const vector<T>& a)
 	cout << endl;
 }
 
-// 앞에 붙는 __global__은 host에서 실행시킬 수 있는 CUDA 커널(kernel) 함수라는 의미입니다.
-// __host__ : CPU에서 호출하고 CPU에서 실행되는 함수
-// __device__ : GPU에서 호출하고 GPU에서 실행되는 함수
-// __host__ __device__ : (함께 사용하면) CPU/GPU 모두에서 실행될 수 있는 함수, 주로 간단한 보조 함수
-__global__ void addKernel(const int* a, const int* b, int* c)
+__global__ void addKernel(const int* a, const int* b, int* c, int size)
 {
-	int i = threadIdx.x;
+	// int i = threadIdx.x;
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
 
-	c[i] = a[i] + b[i];
+	if (i < size)
+		c[i] = a[i] + b[i];
 
-	// 안내: 쿠다에서도 printf()를 사용할 수 있습니다. 기본적인 디버깅에 활용하세요.
-	printf("ThreadIdx(% u, % u, % u)\n", threadIdx.x, threadIdx.y, threadIdx.z);
+	printf("%u %u %u %d\n", blockDim.x, blockIdx.x, threadIdx.x, i);
 }
+
+// 큰 문제에 대해서는
+// addKernel<<<size/numThreadsPerBlock, numThreadsPerBlock>>>(...)
+// addKernel<<<1, 나머지>>>(...)
+// 이렇게 커널 호출 자체를 두 번 하는 것 보다 if(i < size) 같이 조건을 걸어주는 것이 (보통) 더 빠르고 편합니다.
 
 int main()
 {
-	int size = 10; // 블럭(block) 하나만으로 계산할 수 있는 크기 = deviceProp.maxThreadsPerBlock = 1024
+	const int size = 1024 * 1024 * 256; // 여기서는 블럭을 여러 개 사용해야 하는 큰 size
+	//const int size = 37;
+	//const int size = 8;
+
+	// 생각해볼 점: 블럭이 몇 개가 필요할까?
 
 	vector<int> a(size);
 	vector<int> b(size);
-	vector<int> cSingle(size); // 결과 확인용
-	vector<int> c(size, -1);    // CUDA에서 계산한 결과 저장
+	vector<int> c_single(size);     // 결과 확인용
+	vector<int> c(size, -1); // CUDA에서 계산한 결과 저장
 
 	for (int i = 0; i < size; i++)
 	{
 		a[i] = rand() % 10;
 		b[i] = rand() % 10;
-		cSingle[i] = a[i] + b[i];
+		c_single[i] = a[i] + b[i];
 	}
 
 	cout << "Add vectors using CUDA" << endl;
@@ -58,26 +64,36 @@ int main()
 		cudaMalloc((void**)&dev_b, size * sizeof(int)); // input b
 		cudaMalloc((void**)&dev_c, size * sizeof(int)); // output c
 
+		cudaEvent_t start, stop;// 시간 측정을 위한 CUDA 이벤트 생성 (시간측정도 Nsight로 할 수 있습니다.)
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+		cudaEventRecord(start, 0); // 시작 시간 기록
+
 		cudaMemcpy(dev_a, a.data(), size * sizeof(int), cudaMemcpyHostToDevice);
 		cudaMemcpy(dev_b, b.data(), size * sizeof(int), cudaMemcpyHostToDevice);
 
-		// 블럭 1개 * 쓰레드 size개
-		addKernel << <1, size >> > (dev_a, dev_b, dev_c);
-		// addKernel<<<블럭이 몇 개 인지, 각 블럭당 쓰레드가 몇 개인지 >>>(dev_a, dev_b, dev_c);
+		const int threadsPerBlock = 1024; // 최대 deviceProp.maxThreadsPerBlock = 1024 까지 가능
+		int  blocks = int(ceil(float(size)/threadsPerBlock)); // 블럭 여러 개 사용
+		addKernel <<<blocks, threadsPerBlock>>> (dev_a, dev_b, dev_c, size);
 
-		// 안내:
-		// - cudaMemcpy()와 달리 커널 호출은 항상 비동기적(asynchronous)입니다. 
-		// - GPU에게 명령만 내리고 CPU는 바로 다음 명령을 수행한다는 의미입니다.
-		// - CPU에게 GPU가 일을 다 끝날때까지 강제로 기다리게 하고 싶다면 아래의 
-		// - cudaDeviceSynchronize()를 사용할 수 있습니다.
-		// - 함수 이름에서 볼 수 있듯이, 이렇게 기다리는 것을 "동기화(synchronize)"라고 합니다.
+		// addKernel << <2, 4 >> > (dev_a, dev_b, dev_c, size);
 
-		cudaDeviceSynchronize();       // kernel이 끝날때까지 대기 (동기화)
-
-		// 안내: kernel 실행 후 cudaGetLastError() 생략하였습니다.
+		// 안내: kernel 실행 후 cudaGetLastError() 생략
 
 		// 결과 복사 device -> host
 		cudaMemcpy(c.data(), dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+
+		cudaEventRecord(stop, 0);  // 끝나는 시간 기록
+
+		cudaDeviceSynchronize();       // kernel이 끝날때까지 대기 (동기화)
+		// cudaEventSynchronize(stop); // 불필요 (동기화 중복)
+
+		float milliseconds = 0;
+		cudaEventElapsedTime(&milliseconds, start, stop); // 걸린 시간 계산
+		cout << "Time elapsed: " << milliseconds << " ms" << endl;
+
+		cudaEventDestroy(start);
+		cudaEventDestroy(stop);
 
 		cudaFree(dev_c);
 		cudaFree(dev_a);
@@ -89,12 +105,12 @@ int main()
 	if (size < 40) { // size가 작을 경우에는 출력해서 확인
 		printVector(a);
 		printVector(b);
-		printVector(cSingle);
+		printVector(c_single);
 		printVector(c);
 	}
 
 	for (int i = 0; i < size; i++)
-		if (cSingle[i] != c[i])
+		if (c_single[i] != c[i])
 		{
 			cout << "Wrong result" << endl;
 			return 1;
@@ -104,3 +120,4 @@ int main()
 
 	return 0;
 }
+
